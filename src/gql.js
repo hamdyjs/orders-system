6c528ef30c7787e422b9cb9c63dadfac1610179b
Ahmed Hamdy <hamdyjs@gmail.com>
2019-04-29 21:13:54 +0200

    Implement restricting getBuyerOrders to authorized buyers only

--- src/gql.js
+++ src/gql.js
@@ -1,4 +1,5 @@
 const graphql = require("graphql");
+var tokens = require("./tokens");
 
 var Product = require("./product.model");
 var Buyer = require("./buyer.model");
@@ -9,20 +10,21 @@ var schema = graphql.buildSchema(`
         getProducts: [Product!],
         getBuyers: [Buyer!],
         getOrders: [Order!],
-        getOrder: Order
+        getBuyerOrders(buyerID: String!): [Order]
     },
     type Mutation {
-        createProduct(name: String!) : Product,
-        createBuyer(name: String!) : Buyer,
-        createOrder(buyerId: String!, productId: String!) : Order
+        createProduct(productName: String!) : Product,
+        createBuyer(buyerName: String!) : Buyer,
+        createOrder(buyerID: String!, productID: String!) : Order
     },
     type Product {
         id: String,
-        name: String
+        productName: String
     },
     type Buyer {
         id: String,
-        name: String
+        buyerName: String,
+        buyerAuthToken: String,
     },
     type Order {
         id: String,
@@ -42,23 +44,36 @@ var root = {
     getOrders: function() {
         return Order.find().exec();
     },
-    getOrder: function(args) {
-        return null;
+    getBuyerOrders: function(args, context) {
+        var token = context.headers.authorization;
+        if (token) {
+            token = token.substring(4);
+            var payload = tokens.verify(token);
+            if (payload && args.buyerID == payload.id) {
+                return Order.find({buyer: args.buyerID}).exec();
+            } else {
+                throw new Error("Wrong authorization token");
+            }
+        } else {
+            throw new Error("Not authorized");
+        }
     },
     createProduct: function(args) {
-        var p = new Product({name: args.name});
+        var p = new Product({productName: args.productName});
         p.save();
         return p;
     },
     createBuyer: function(args) {
-        var b = new Buyer({name: args.name});
+        var b = new Buyer({buyerName: args.buyerName});
+        var token = tokens.sign({id: b.id});
+        b.buyerAuthToken = token;
         b.save();
         return b;
     },
     createOrder: function(args) {
         var o = new Order({
-            buyer: args.buyerId,
-            product: args.productId,
+            buyer: args.buyerID,
+            product: args.productID,
         });
         o.save();
         return o;
